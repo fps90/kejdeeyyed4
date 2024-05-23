11960b860fc2a136054d6196febf15962c648e47
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-24 01:43:32 +0300

    Update inline.py

--- YukkiMusic/cplugin/utils/inline.py
+++ YukkiMusic/cplugin/utils/inline.py
@@ -27,6 +27,36 @@ buttons = InlineKeyboardMarkup(
     ]
 )
 
-close_key = InlineKeyboardMarkup(
-    [[InlineKeyboardButton(text="✯ ᴄʟᴏsᴇ ✯", callback_data="close")]]
-)
+close_key = [
+
+        [
+            
+            InlineKeyboardButton(
+                text="𝗘𝗻𝗱 🎸•",
+                callback_data="end_cb"
+            ),
+            InlineKeyboardButton(
+                text="𝗣𝗮𝘂𝘀𝗲 🎸•",
+                callback_data="pause_cb"
+            ),
+            InlineKeyboardButton(
+                text="𝗥𝗲𝘀𝘂𝗺𝗲 🎸•",
+                callback_data="resume_cb",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"downloadvideo {videoid}"),
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"downloadaudio {videoid}")
+        ],
+        [
+            InlineKeyboardButton(
+                text="زیادم بکە بۆ گرووپ یان کەناڵت ⚡️•",
+                callback_data=f"url=f"https://t.me/{viv.username}?startgroup=true",
+            ),
+        ],
+    ]
+
