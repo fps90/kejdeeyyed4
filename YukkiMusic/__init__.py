bae03d906e5cc99b8df4002ffbc4da353f85b4ed
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-23 13:35:01 +0300

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
 # This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
@@ -7,56 +6,201 @@
 #
 # All rights reserved.
 #
+import re
+import sys
+import config
+import asyncio
+import importlib
+from sys import argv
 
-from telethon import TelegramClient
+from pyrogram import idle, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import BANNED_USERS, OWNER_ID
 
-from config import API_ID, API_HASH
-from YukkiMusic.core.bot import YukkiBot
-from YukkiMusic.core.dir import dirr
-from YukkiMusic.core.git import git
-from YukkiMusic.core.userbot import Userbot
-from YukkiMusic.misc import dbb, heroku, sudo
-from SafoneAPI import SafoneAPI
+from YukkiMusic import LOGGER, app, userbot
+from YukkiMusic import telethn
+from YukkiMusic.core.call import Yukki
+from YukkiMusic.plugins import ALL_MODULES
+from YukkiMusic.utils.database import get_banned_users, get_gbanned
+from YukkiMusic.utils.decorators.language import LanguageStart
+from YukkiMusic.utils.inlinefunction import paginate_modules
+from YukkiMusic.utils.inline import private_panel
 
+# from YukkiMusic.plugins.tools.clone import restart_bots
 
-from .logging import LOGGER
-
-# Directories
-dirr()
-
-# Check Git Updates
-git()
-
-# Initialize Memory DB
-dbb()
-
-# Heroku APP
-heroku()
-
-# Load Sudo Users from DB
-sudo()
-api = SafoneAPI()
-# Bot Client
-app = YukkiBot()
-
-# Assistant Client
-userbot = Userbot()
-
-### Modules
-MOD_LOAD = []
-MOD_NOLOAD = []
-
-from .platforms import *
-
-YouTube = YouTubeAPI()
-Carbon = CarbonAPI()
-Spotify = SpotifyAPI()
-Apple = AppleAPI()
-Resso = RessoAPI()
-SoundCloud = SoundAPI()
-Telegram = TeleAPI()
+loop = asyncio.get_event_loop_policy().get_event_loop()
+HELPABLE = {}
 
 
-TEMP_DOWNLOAD_DIRECTORY = "downloads"
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("YukkiMusic").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
+        LOGGER("YukkiMusic").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        imported_module = importlib.import_module("YukkiMusic.plugins" + all_module)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            imported_module.__MODULE__ = imported_module.__MODULE__
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("Yukkimusic.plugins").info("Successfully Imported Modules ")
+    # await restart_bots()
+    await userbot.start()
+    await Yukki.start()
+    await Yukki.decorators()
+    LOGGER("YukkiMusic").info("Yukki Music Bot Started Successfully")
+    await idle()
+    if len(argv) not in (1, 3, 4):
+        await telethn.disconnect()
+    else:
+        await telethn.run_until_disconnected()
 
-telethn = TelegramClient("YukkiMusic", API_ID, API_HASH)
+
+async def help_parser(name, keyboard=None):
+    if not keyboard:
+        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
+    return (
+        """ʜᴇʟʟᴏ {first_name},
+
+ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
+
+ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs sᴛᴀʀᴛs ᴡɪᴛʜ :-  /
+""".format(
+            first_name=name
+        ),
+        keyboard,
+    )
+
+
+@app.on_callback_query(filters.regex("shikharbro"))
+async def shikhar(_, CallbackQuery):
+    text, keyboard = await help_parser(CallbackQuery.from_user.mention)
+    await CallbackQuery.message.edit(text, reply_markup=keyboard)
+
+
+@app.on_callback_query(filters.regex(r"help_(.*?)"))
+@LanguageStart
+async def help_button(client, query, _):
+    home_match = re.match(r"help_home\((.+?)\)", query.data)
+    mod_match = re.match(r"help_module\((.+?),(.+?)\)", query.data)
+    prev_match = re.match(r"help_prev\((.+?)\)", query.data)
+    next_match = re.match(r"help_next\((.+?)\)", query.data)
+    back_match = re.match(r"help_back\((\d+)\)", query.data)
+    create_match = re.match(r"help_create", query.data)
+
+    top_text = f"""ʜᴇʟʟᴏ {query.from_user.first_name},
+
+ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
+
+ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs sᴛᴀʀᴛs ᴡɪᴛʜ :-  /
+"""
+
+    if mod_match:
+        module = mod_match.group(1)
+        prev_page_num = int(mod_match.group(2))
+        text = (
+            "{} **{}**:\n".format(
+                "**ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ**", HELPABLE[module].__MODULE__
+            )
+            + HELPABLE[module].__HELP__
+        )
+        try:
+            await app.resolve_peer(OWNER_ID[0])
+            OWNER = OWNER_ID[0]
+        except:
+            OWNER = None
+        out = private_panel(_, app.username, OWNER)
+
+        key = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="↪️ Back", callback_data=f"help_back({prev_page_num})"
+                    ),
+                    InlineKeyboardButton(text="🔄 Close", callback_data="close"),
+                ],
+            ]
+        )
+
+        await query.message.edit(
+            text=text,
+            reply_markup=key,
+            disable_web_page_preview=True,
+        )
+
+    elif home_match:
+        await app.send_message(
+            query.from_user.id,
+            text=home_text_pm,
+            reply_markup=InlineKeyboardMarkup(out),
+        )
+        await query.message.delete()
+
+    elif prev_match:
+        curr_page = int(prev_match.group(1))
+        if curr_page < 0:
+            curr_page = max_num_pages - 1
+        await query.message.edit(
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_modules(curr_page, HELPABLE, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif next_match:
+        next_page = int(next_match.group(1))
+        await query.message.edit(
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_modules(next_page, HELPABLE, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif back_match:
+        prev_page_num = int(back_match.group(1))
+        await query.message.edit(
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_modules(prev_page_num, HELPABLE, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif create_match:
+        text, keyboard = await help_parser(query)
+        await query.message.edit(
+            text=text,
+            reply_markup=keyboard,
+            disable_web_page_preview=True,
+        )
+
+    return await client.answer_callback_query(query.id)
+
+
+if __name__ == "__main__":
+    telethn.start(bot_token=config.BOT_TOKEN)
+    loop.run_until_complete(init())
+    LOGGER("YukkiMusic").info("Stopping Yukki Music Bot! GoodBye")
