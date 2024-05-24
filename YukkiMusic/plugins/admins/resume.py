c4a727fa7bcb2ed73376ce7caa391277d85561d6
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-25 01:20:51 +0300

    Update resume.py

--- YukkiMusic/plugins/admins/resume.py
+++ YukkiMusic/plugins/admins/resume.py
@@ -10,19 +10,18 @@
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from YukkiMusic.utils.inline import close_markup
 from config import BANNED_USERS
-from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import is_music_playing, music_on
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
-# Commands
-RESUME_COMMAND = get_command("RESUME_COMMAND")
 
 
-@app.on_message(filters.command(RESUME_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(["resume", "cresume","د","دەستپێکردنەوە"], prefixes=["/", "!", "%", "", ".", "@", "#"]) & ~filters.private & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
@@ -31,4 +30,6 @@ async def resume_com(cli, message: Message, _, chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
     await Yukki.resume_stream(chat_id)
-    await message.reply_text(_["admin_4"].format(message.from_user.mention))
+    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
+    await message.reply_text(_["admin_4"].format(user_mention), reply_markup=close_markup(_)
+    )
