8d789f4e23fef95d2fc5afd5c7455e42a41a4537
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-25 01:01:04 +0300

    Update play.py

--- YukkiMusic/cplugin/play.py
+++ YukkiMusic/cplugin/play.py
@@ -164,7 +164,7 @@ async def play(client, message: Message):
         except:
             pass
 
-    ruser = message.from_user.first_name
+    ruser = message.from_user.mention
     audio = (
         (message.reply_to_message.audio or message.reply_to_message.voice)
         if message.reply_to_message
