3f10185233d3dd1dff4beade8f2062f6c2f85a71
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-25 01:18:14 +0300

    Update pause.py

--- YukkiMusic/plugins/admins/pause.py
+++ YukkiMusic/plugins/admins/pause.py
@@ -10,19 +10,17 @@
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from YukkiMusic.utils.inline import close_markup
 from config import BANNED_USERS
-from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import is_music_playing, music_off
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
-# Commands
-PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
 
-@app.on_message(filters.command(PAUSE_COMMAND) & filters.group & ~BANNED_USERS)
+
+@app.on_message(filters.command(["pause", "cpause","وسبە","وەستانی کاتی"], prefixes=["/", "@", ".", "#",""]) & ~filters.private & ~BANNED_USERS)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
@@ -30,5 +28,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
     await Yukki.pause_stream(chat_id)
-    await message.reply_text(_["admin_2"].format(message.from_user.mention))
+    await message.reply_text(_["admin_2"].format(user_mention), reply_markup=close_markup(_)
+    )
