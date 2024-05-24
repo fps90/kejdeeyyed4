076fbd14f966022b7129357e6ce449dc73598cb0
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-24 23:52:27 +0300

    Update id.py

--- YukkiMusic/plugins/tools/id.py
+++ YukkiMusic/plugins/tools/id.py
@@ -1,40 +1,55 @@
-from pyrogram import filters
 from YukkiMusic import app
+from pyrogram import Client, filters
 
+from pyrogram.enums import ParseMode
 
-@app.on_message(filters.command("id"))
-async def get_id(client, message):
-    try:
+####
 
-        if (not message.reply_to_message) and (message.chat):
-            await message.reply(
-                f"User {message.from_user.first_name}'s ID is <code>{message.from_user.id }</code>.\nThis chat's ID is: <code>{message.chat.id}</code>."
-            )
+@app.on_message(filters.command('id'))
+async def getid(client, message):
+    chat = message.chat
+    your_id = message.from_user.id
+    message_id = message.id
+    reply = message.reply_to_message
 
-        elif not message.reply_to_message:
-            await message.reply(
-                f"User {message.from_user.first_name}'s ID is <code>{message.from_user.id }</code>."
-            )
+    text = f"**[ئایدی نامە::]({message.link})** `{message_id}`\n"
+    text += f"**[ئایدییەکەت:](tg://user?id={your_id})** `{your_id}`\n"
 
-        elif message.reply_to_message.forward_from_chat:
-            await message.reply(
-                f"The forwarded {str(message.reply_to_message.forward_from_chat.type)[9:].lower()}, {message.reply_to_message.forward_from_chat.title} has an ID of <code>{message.reply_to_message.forward_from_chat.id}</code>."
-            )
+    if not message.command:
+        message.command = message.text.split()
 
-        elif message.reply_to_message.forward_from:
-            await message.reply(
-                f"The forwarded user, {message.reply_to_message.forward_from.first_name} has an ID of <code>{message.reply_to_message.forward_from.id   }</code>."
-            )
+    if not message.command:
+        message.command = message.text.split()
 
-        elif message.reply_to_message.forward_sender_name:
-            await message.reply(
-                "Sorry, i never seen that's user message or user do i am unable to fetch id"
-            )
+    if len(message.command) == 2:
+        try:
+            split = message.text.split(None, 1)[1].strip()
+            user_id = (await client.get_users(split)).id
+            text += f"**[ئایدی بەکارهێنەر:](tg://user?id={user_id})** `{user_id}`\n"
 
-        else:
-            await message.reply(
-                f"User {message.reply_to_message.from_user.first_name}'s ID is <code>{message.reply_to_message.from_user.id}</code>."
-            )
+        except Exception:
+            return await message.reply_text("**بەکارهێنەر نییە**", quote=True)
 
-    except Exception:
-        await message.reply("An error occured while getting the ID.")
+    text += f"**[ئایدی گرووپ::](https://t.me/{chat.username})** `{chat.id}`\n\n"
+
+    if (
+        not getattr(reply, "empty", True)
+        and not message.forward_from_chat
+        and not reply.sender_chat
+    ):
+        text += f"**[ئایدی نامەی کەسی بەرامبەر:]({reply.link})** `{reply.id}`\n"
+        text += f"**[ئایدی کەسی بەرامبەر:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+
+    if reply and reply.forward_from_chat:
+        text += f"کەناڵی فۆروارد کراو, {reply.forward_from_chat.title}, ئایدی: `{reply.forward_from_chat.id}`\n\n"
+        print(reply.forward_from_chat)
+
+    if reply and reply.sender_chat:
+        text += f"ئایدی گرووپ یان کەناڵی ڕپلەی کراو: `{reply.sender_chat.id}`"
+        print(reply.sender_chat)
+
+    await message.reply_text(
+        text,
+        disable_web_page_preview=True,
+        parse_mode=ParseMode.DEFAULT,
+    )
