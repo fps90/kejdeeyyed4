ebcd51bdf3922656fe03552916d132638241ae74
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-24 23:27:32 +0300

    Update info.py

--- YukkiMusic/plugins/tools/info.py
+++ YukkiMusic/plugins/tools/info.py
@@ -84,7 +84,7 @@ async def get_chat_info(chat):
     return info, photo_id
 
 
-@app.on_message(filters.command("info"))
+@app.on_message(filters.command("infoo"))
 async def info_func(_, message: Message):
     if message.reply_to_message:
         user = message.reply_to_message.from_user.id
