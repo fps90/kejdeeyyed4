20ec8b7e49a5941b555d01d94af2ba057533609f
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-25 01:22:26 +0300

    Update stop.py

--- YukkiMusic/plugins/admins/stop.py
+++ YukkiMusic/plugins/admins/stop.py
@@ -10,23 +10,29 @@
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from YukkiMusic.utils.inline import close_markup
 from config import BANNED_USERS
-from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import set_loop
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
-# Commands
-STOP_COMMAND = get_command("STOP_COMMAND")
+# Command
 
-
-@app.on_message(filters.command(STOP_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(
+        ["end", "stop", "cend", "cstop","ڕاگرتن","وەستان"],
+        prefixes=["/", "!", "%", ",", "", ".", "@", "#"],
+    )
+    & ~filters.private
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
     await Yukki.stop_stream(chat_id)
     await set_loop(chat_id, 0)
-    await message.reply_text(_["admin_9"].format(message.from_user.mention))
+    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
+    await message.reply_text(_["admin_9"].format(user_mention), reply_markup=close_markup(_)
+    )
