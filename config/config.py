961df20e649013ad93397d51ac74d912016ed8fd
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-25 12:45:29 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -37,7 +37,7 @@ ASSISTANT_PREFIX = getenv("ASSISTANT_PREFIX", ".")
 # ________________________________________________________________________________#
 # Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
+DATABASE_NAME = getenv("DATABASE_NAME", "YukkiMusic")
 
 # ________________________________________________________________________________#
 # Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
