c36872aa0833cbec21c144f7f9b3f9daa672c3e3
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-25 11:08:49 +0300

    Update filtersdb.py

--- YukkiMusic/mongo/filtersdb.py
+++ YukkiMusic/mongo/filtersdb.py
@@ -2,122 +2,72 @@ from YukkiMusic.utils.mongo import db
 
 filters = db.filters["filters"] 
 
-async def add_filter_db(chat_id: int, filter_name: str, content: str, text: str, data_type: int):
-   filter_data = await filters.find_one(
-      {
-         'chat_id': chat_id
-      }
-   )
+async def add_filter(grp_id, text, reply_text, btn, file, alert):
+    mycol = filters[str(grp_id)]
+    # mycol.create_index([('text', 'text')])
 
-   if filter_data is None:
-      _id = await filters.count_documents({}) + 1
-      await filters.insert_one(
-         {
-            '_id': _id,
-            'chat_id': chat_id,
-            'filters': [
-               {
-                  'filter_name': filter_name,
-                  'content': content,
-                  'text': text,
-                  'data_type': data_type
-               }
-            ]
-         }
-      )
-   
-   else:
-         FILTERS_NAME = await get_filters_list(chat_id)
-         if filter_name not in FILTERS_NAME:
-            await filters.update_one(
-               {
-                  'chat_id': chat_id
-               },
-               {
-                  '$addToSet': {
-                     'filters': {
-                        'filter_name': filter_name,
-                        'content': content,
-                        'text': text,
-                        'data_type': data_type
-                     }
-                  }
-               },
-               upsert=True
-            )
-         else:
-            await filters.update_one(
-               {
-                  'chat_id': chat_id,
-                  'filters.filter_name': filter_name
-               },
-               {
-                  '$set': {
-                     'filters.$.filter_name': filter_name,
-                     'filters.$.content': content,
-                     'filters.$.text': text,
-                     'filters.$.data_type': data_type
-                  }
-               }
-            )
+    data = {
+        'text':str(text),
+        'reply':str(reply_text),
+        'btn':str(btn),
+        'file':str(file),
+        'alert':str(alert)
+    }
 
-async def stop_db(chat_id: int, filter_name:str):
-   await filters.update_one(
-      {
-         'chat_id': chat_id
-      },
-      {
-         '$pull': {
-            'filters': {
-               'filter_name': filter_name
-            }
-         }
-      }
-   )
+    try:
+        mycol.update_one({'text': str(text)},  {"$set": data}, upsert=True)
+    except:
+        logger.exception('Some error occured!', exc_info=True)
+             
+     
 
-async def stop_all_db(chat_id: id):
-   await filters.update_one(
-      {
-         'chat_id': chat_id
-      },
-      {
-         '$set': {
-            'filters': []
-         }
-      },
-      upsert=True
-   )
-   
-async def get_filter(chat_id: int, filter_name: str):
-   filter_data = await filters.find_one(
-      {
-         'chat_id': chat_id
-      }
-   )
-   if filter_data is not None:
-      filters_ = filter_data['filters']
-      for filter_ in filters_:
-         if filter_['filter_name'] == filter_name:
-            content = filter_['content']
-            text = filter_['text']
-            data_type = filter_['data_type']
-            return (
-               filter_name,
-               content,
-               text,
-               data_type
-            )
 
-async def get_filters_list(chat_id: int):
-   filter_data = await filters.find_one(
-      {
-         'chat_id': chat_id
-      }
-   )
-   if filter_data is not None:
-      FILTERS_NAME = list()
-      for filter_name in filter_data['filters']:
-         FILTERS_NAME.append(filter_name['filter_name'])
-      return FILTERS_NAME
-   else:
-      return []
+async def get_filters(group_id):
+    mycol = filters[str(group_id)]
+
+    texts = []
+    query = mycol.find()
+    try:
+        for file in query:
+            text = file['text']
+            texts.append(text)
+    except:
+        pass
+    return texts
+
+
+async def delete_filter(message, text, group_id):
+    mycol = filters[str(group_id)]
+    
+    myquery = {'text':text }
+    query = mycol.count_documents(myquery)
+    if query == 1:
+        mycol.delete_one(myquery)
+        await message.reply_text(
+            f"'`{text}`'  deleted. I'll not respond to that filter anymore.",
+            quote=True,
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
+    else:
+        await message.reply_text("Couldn't find that filter!", quote=True)
+
+
+async def del_all(message, group_id, title):
+    if str(group_id) not in filters.list_collection_names():
+        await message.edit_text(f"Nothing to remove in {title}!")
+        return
+
+    mycol = filters[str(group_id)]
+    try:
+        mycol.drop()
+        await message.edit_text(f"All filters from {title} has been removed")
+    except:
+        await message.edit_text("Couldn't remove all filters from group!")
+        return
+
+
+async def count_filters(group_id):
+    mycol = filters[str(group_id)]
+
+    count = mycol.count()
+    return False if count == 0 else count
