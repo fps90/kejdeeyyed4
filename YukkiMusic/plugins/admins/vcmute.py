fbed99a2ca0a982daaac6f415c26778add13a32b
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-26 08:53:44 +0300

    Update vcmute.py

--- YukkiMusic/plugins/admins/vcmute.py
+++ YukkiMusic/plugins/admins/vcmute.py
@@ -12,12 +12,13 @@ from pyrogram import filters
 from pyrogram.types import Message
 from config import BANNED_USERS
 from YukkiMusic import app
+from YukkiMusic.utils.inline import close_markup
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import is_muted, mute_on, mute_off
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
 
-@app.on_message(filters.command(["vcmute"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["vcmute"]) & ~filters.private & ~BANNED_USERS)
 @AdminRightsCheck
 async def mute_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
@@ -26,12 +27,12 @@ async def mute_admin(cli, message: Message, _, chat_id):
         return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
     await mute_on(chat_id)
     await Yukki.mute_stream(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
     await message.reply_text(
-        _["admin_6"].format(message.from_user.mention), disable_web_page_preview=True
+        _["admin_6"].format(user_mention), reply_markup=close_markup(_)
     )
 
-
-@app.on_message(filters.command(["vcunmute"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["vcunmute"]) & ~filters.private & ~BANNED_USERS)
 @AdminRightsCheck
 async def unmute_admin(Client, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
@@ -40,6 +41,7 @@ async def unmute_admin(Client, message: Message, _, chat_id):
         return await message.reply_text(_["admin_7"], disable_web_page_preview=True)
     await mute_off(chat_id)
     await Yukki.unmute_stream(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
     await message.reply_text(
-        _["admin_8"].format(message.from_user.mention), disable_web_page_preview=True
+        _["admin_8"].format(user_mention), reply_markup=close_markup(_)
     )
