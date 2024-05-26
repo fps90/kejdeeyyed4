082596db292ff9a3f5ffb824205585c49238f804
j0g5gd8798ly <saroksurarazo@gmail.com>
2024-05-26 08:38:33 +0300

    Update tagall.py

--- YukkiMusic/plugins/tools/tagall.py
+++ YukkiMusic/plugins/tools/tagall.py
@@ -5,14 +5,14 @@ from pyrogram.enums import ChatType, ChatMemberStatus, ChatMembersFilter
 from pyrogram.errors import UserNotParticipant, FloodWait
 from pyrogram.types import ChatPermissions
 from YukkiMusic import app
-from YukkiMusic.utils.filter import admin_filter
-
+from YukkiMusic.utils.alina_ban import admin_filter
+from YukkiMusic.utils.database import get_assistant
 
 SPAM_CHATS = []
 
 
 @app.on_message(
-    filters.command(["all", "mention", "mentionall", "tagall"], prefixes=["/", "@"])
+    filters.command(["all", "allmention", "mentionall", "tagall",""], prefixes=["/", "@"])
     & admin_filter
 )
 async def tag_all_users(_, message):
@@ -37,7 +37,11 @@ async def tag_all_users(_, message):
                 usernum += 1
                 usertxt += f"[{m.user.first_name}](tg://user?id={m.user.id})"
                 if usernum == 7:
-                    await replied.reply_text(usertxt)
+                    await app.send_message(
+                        message.chat.id,
+                        f"{replied.text}\n\n{usertxt}",
+                        disable_web_page_preview=True,
+                    )
                     await asyncio.sleep(1)
                     usernum = 0
                     usertxt = ""
@@ -63,6 +67,7 @@ async def tag_all_users(_, message):
                     await app.send_message(
                         message.chat.id,
                         f"{text}\n{usertxt}",
+                        disable_web_page_preview=True,
                     )
                     await asyncio.sleep(2)
                     usernum = 0
@@ -103,7 +108,11 @@ async def tag_all_users(_, message):
                 usernum += 1
                 usertxt += f"[{m.user.first_name}](tg://user?id={m.user.id})"
                 if usernum == 7:
-                    await replied.reply_text(usertxt)
+                    await app.send_message(
+                        message.chat.id,
+                        f"{replied.text}\n\n usertxt",
+                        disable_web_page_preview=True,
+                    )
                     await asyncio.sleep(1)
                     usernum = 0
                     usertxt = ""
@@ -129,12 +138,78 @@ async def tag_all_users(_, message):
                     await app.send_message(
                         message.chat.id,
                         f"{text}\n{usertxt}",
+                        disable_web_page_preview=True,
                     )
                     await asyncio.sleep(2)
                     usernum = 0
                     usertxt = ""
         except FloodWait as e:
             await asyncio.sleep(e.value)
+        try:
+            SPAM_CHATS.remove(message.chat.id)
+        except Exception:
+            pass
+
+
+@app.on_message(
+    filters.command(
+        ["atag", "aall", "amention", "amentionall"], prefixes=["/", "@", ".", "#"]
+    )
+    & admin_filter
+)
+async def tag_all_useres(_, message):
+    userbot = await get_assistant(message.chat.id)
+    if message.chat.id in SPAM_CHATS:
+        return await message.reply_text(
+            "ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss ɪs ᴀʟʀᴇᴀᴅʏ ʀᴜɴɴɪɴɢ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴛᴏᴘ sᴏ ᴜsᴇ /acancel"
+        )
+    replied = message.reply_to_message
+    if len(message.command) < 2 and not replied:
+        await message.reply_text(
+            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@aall Hi Friends`"
+        )
+        return
+    if replied:
+        SPAM_CHATS.append(message.chat.id)
+        usernum = 0
+        usertxt = ""
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
+                break
+            usernum += 1
+            usertxt += f"[{m.user.first_name}](tg://openmessage?user_id={m.user.id})"
+            if usernum == 7:
+                await userbot.send_message(
+                    message.chat.id,
+                    f"{replied.text}\n\n{usertxt}",
+                    disable_web_page_preview=True,
+                )
+                await asyncio.sleep(2)
+                usernum = 0
+                usertxt = ""
+        try:
+            SPAM_CHATS.remove(message.chat.id)
+        except Exception:
+            pass
+    else:
+        text = message.text.split(None, 1)[1]
+
+        SPAM_CHATS.append(message.chat.id)
+        usernum = 0
+        usertxt = ""
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
+                break
+            usernum += 1
+            usertxt += f'<a href="tg://openmessage?user_id={m.user.id}">{m.user.first_name}</a>'
+
+            if usernum == 7:
+                await userbot.send_message(
+                    message.chat.id, f"{text}\n{usertxt}", disable_web_page_preview=True
+                )
+                await asyncio.sleep(2)
+                usernum = 0
+                usertxt = ""
         try:
             SPAM_CHATS.remove(message.chat.id)
         except Exception:
@@ -151,7 +226,7 @@ async def tag_all_users(_, message):
             "mentionoff",
             "cancelall",
         ],
-        prefixes=["/", "@", "#"],
+        prefixes=["/", "@"],
     )
     & admin_filter
 )
@@ -167,3 +242,19 @@ async def cancelcmd(_, message):
     else:
         await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")
         return
+
+
+__MODULE__ = "Tᴀɢᴀʟʟ"
+__HELP__ = """
+
+@all ᴏʀ /all | /tagall ᴏʀ  @tagall | /mentionall ᴏʀ  @mentionall [ᴛᴇxᴛ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇssᴀɢᴇ] ᴛᴏ ᴛᴀɢ ᴀʟʟ ᴜsᴇʀ's ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙᴛ ʙᴏᴛ
+
+@aall ᴏʀ /aall | /atagall ᴏʀ  @atagall | /amentionall ᴏʀ  @amentionall  [ᴛᴇxᴛ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇssᴀɢᴇ] ᴛᴏ ᴛᴀɢ ᴀʟʟ ᴜsᴇʀ's ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙʏ ʙᴏᴛ's ᴀssɪsᴛᴀɴᴛ
+
+
+/admin ᴏʀ @admin | /admintag ᴏʀ @admintag | /tagadmin ᴏʀ @tagadmin | /admins Oʀ @admins [ᴛᴇxᴛ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇssᴀɢᴇ] ᴛᴏ ᴛᴀɢ ᴀʟʟ ᴀᴅᴍɪɴ's ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ
+
+/cancel Oʀ @cancel |  /offmention Oʀ @offmention | /mentionoff Oʀ @mentionoff | /cancelall Oʀ @cancelall - ᴛᴏ sᴛᴏᴘ ʀᴜɴɴɪɴɢ ᴀɴʏ ᴛᴀɢ ᴘʀᴏᴄᴇss
+
+**__Nᴏᴛᴇ__** Tʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴄᴀɴ ᴏɴʟʏ ᴜsᴇ ᴛʜᴇ Aᴅᴍɪɴs ᴏғ Cʜᴀᴛ ᴀɴᴅ ᴍᴀᴋᴇ Sᴜʀᴇ Bᴏᴛ ᴀɴᴅ ᴀssɪsᴛᴀɴᴛ ɪs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ's
+"""
